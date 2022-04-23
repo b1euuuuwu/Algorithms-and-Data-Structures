603d288b3ba138b0c8144be1be60bdd4b9d7c9b7
Almas Mirzandi <88608985+b1euuuuwu@users.noreply.github.com>
2022-04-23 01:22:03 -0400

    Assignment 7

---
+++ Assignment 7/src/dijkstrasalgo/Edge.java
@@ -0,0 +1,24 @@
+package dijkstrasalgo;
+
+public class Edge {
+	int source, dest, weight;
+
+	public Edge(int source, int dest, int weight)
+	{
+		this.source = source;
+		this.dest = dest;
+		this.weight = weight;
+	}
+}
+
+// A class to store a heap node
+class Node
+{
+	int vertex, weight;
+
+	public Node(int vertex, int weight)
+	{
+		this.vertex = vertex;
+		this.weight = weight;
+	}
+}
---
+++ Assignment 7/src/dijkstrasalgo/Graph.java
@@ -0,0 +1,24 @@
+package dijkstrasalgo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+	// A list of lists to represent an adjacency list
+	List<List<Edge>> adjList = null;
+
+	// Constructor
+	Graph(List<Edge> edges, int n)
+	{
+		adjList = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			adjList.add(new ArrayList<>());
+		}
+
+		// add edges to the directed graph
+		for (Edge edge: edges) {
+			adjList.get(edge.source).add(edge);
+		}
+	}
+}
